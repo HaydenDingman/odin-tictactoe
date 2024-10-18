8dca3fcc290e1868374244d09b560cd016228685
Hayden Dingman <haydendingman@gmail.com>
2024-10-17 18:36:59 -0700

    Allow renaming of players and add CSS styling

--- tictactoe.js
+++ tictactoe.js
@@ -85,7 +85,21 @@ const Board = (function () {
 const Display = (function () {
 
     // Cache DOM
-    let container = document.querySelector(".container");
+    const container = document.querySelector(".gameboard");
+    const playerOneName = document.querySelector("#player-one-name");
+    const playerTwoName = document.querySelector("#player-two-name");
+    const editButtons = document.querySelectorAll(".edit-button");
+    const playerOneApply = document.querySelector(".player-one-apply");
+    const playerTwoApply = document.querySelector(".player-two-apply");
+    const resultsContainer = document.querySelector(".results");
+
+
+    // Event Listeners
+    for (const button of editButtons) {
+        button.addEventListener("click", editName);
+    }
+    playerOneApply.addEventListener("click", confirmName);
+    playerTwoApply.addEventListener("click", confirmName);
 
     function render() {
         container.replaceChildren();
@@ -103,36 +117,127 @@ const Display = (function () {
         }
     }
 
-    return { render };
+    function highlightActivePlayer(player) {
+        if (player === "O") {
+            if (playerOneName.disabled === true) {
+                playerOneName.style.borderBottom = "3px solid var(--grid-lines)";
+            }
+            playerTwoName.style.borderBottom = "none";
+        } else if (player === "X") {
+            if (playerTwoName.disabled === true) {
+                playerTwoName.style.borderBottom = "3px solid var(--grid-lines)";
+            }
+            playerOneName.style.borderBottom = "none";
+        }
+    }
+
+    function editName(e) {
+        if (this.classList.contains("player-one-edit")) {
+            playerOneName.disabled = false;
+            playerOneName.focus();
+            this.style.visibility = "hidden";
+            playerOneApply.style.visibility = "visible";
+        } else {
+            playerTwoName.disabled = false;
+            playerTwoName.focus();
+            this.style.visibility = "hidden";
+            playerTwoApply.style.visibility = "visible";
+        }
+    }
+
+    function confirmName(e) {
+        if (this.classList.contains("player-one-edit")) {
+            console.log(playerOneName.value);
+            if (playerOneName.value != "") {
+                Game.playerOne.setName(playerOneName.value);
+            }
+            playerOneName.disabled = true;
+            playerOneApply.style.visibility = "hidden";
+            for (const button of editButtons) {
+                if (button.classList.contains("player-one-edit")) {
+                    button.style.visibility = "visible";
+                }
+            }
+        }
+        else if (this.classList.contains("player-two-edit")) {
+            console.log(playerTwoName.value);
+            if (playerTwoName.value != "") {
+                Game.playerTwo.setName(playerTwoName.value);
+            }
+            playerTwoName.disabled = true;
+            playerTwoApply.style.visibility = "hidden";
+            for (const button of editButtons) {
+                if (button.classList.contains("player-two-edit")) {
+                    button.style.visibility = "visible";
+                }
+            }
+        }
+    }
+
+    function showResults(winner) {
+        if (winner === "X") {
+            resultsContainer.textContent = `${Game.playerOne.getName()} wins! Play again?`
+        } else if (winner === "O") {
+            resultsContainer.textContent = `${Game.playerTwo.getName()} wins! Double or nothing?`
+        } else if (winner === "tie") {
+            resultsContainer.textContent = `You fought to a draw. War is a pointless endeavor.`
+        }
+    }
+
+    function clearResults() {
+        resultsContainer.textContent = "";
+    }
+
+    return { render, showResults, clearResults, highlightActivePlayer };
 })();
 
 const Game = (function () {
 
     function createPlayer(name, marker) {
-        this.name = name;
-        this.marker = marker;
-        return { name, marker };
+        let playerName = name;
+        const playerMarker = marker;
+
+        function setName(newName) {
+            playerName = newName;
+        }
+
+        function getName() {
+            return playerName;
+        }
+
+        function getMarker() {
+            return playerMarker;
+        }
+
+        return { setName, getName, getMarker, playerName };
     };
 
     const playerOne = createPlayer("Player One", "X");
     const playerTwo = createPlayer("Player Two", "O");
     let playerTurn = playerOne;
+    let turnCount = 0;
+    let lastWinner = null;
 
     // Cache DOM
     let newGameButton = document.querySelector(".new-game");
     newGameButton.addEventListener("click", newGame);
 
     function getPlayerTurn() {
-        return playerTurn.marker;
+        return playerTurn.getMarker();
     }
 
     function playerMove(index) {
         if (playerTurn) {
             if (Board.updateBoard(index)) {
+                turnCount++;
                 Display.render();
+                Display.highlightActivePlayer(getPlayerTurn());
                 if (Board.checkBoard()) {
-                    console.log(playerTurn.name + " wins!")
+                    Display.showResults(getPlayerTurn());
+                    lastWinner = playerTurn;
                     playerTurn = null;
+                } else if (turnCount === 9) {
+                    console.log("You tied.");
                 } else {
                     endTurn();
                 }
@@ -150,13 +255,16 @@ const Game = (function () {
     }
 
     function newGame() {
-        console.log("New game");
+        turnCount = 0;
         playerTurn = playerOne;
+
         Board.resetBoard();
+        Display.clearResults();
+        Display.highlightActivePlayer("O");
         Display.render();
     }
 
-    return { getPlayerTurn, playerMove, newGame }
+    return { getPlayerTurn, playerMove, newGame, playerOne, playerTwo, lastWinner, playerTurn }
 })();
 
 Game.newGame();
